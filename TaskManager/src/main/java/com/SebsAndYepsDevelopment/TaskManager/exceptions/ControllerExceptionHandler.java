c462e3206addc24d81df0016d25e0de17aff5102
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-12-11 18:16:42 -0500

    Remade the ControllerExceptionHandler just to solidify knowledge

--- TaskManager/src/main/java/com/SebsAndYepsDevelopment/TaskManager/exceptions/ControllerExceptionHandler.java
+++ TaskManager/src/main/java/com/SebsAndYepsDevelopment/TaskManager/exceptions/ControllerExceptionHandler.java
@@ -8,16 +8,15 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> taskNotFoundException(TaskNotFoundException e, WebRequest webRequest)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException e, WebRequest webRequest)
     {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> userNotFoundException(UserNotFoundException e, WebRequest webRequest)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e, WebRequest webRequest)
     {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
