1032f0984ad373fdf0c729bc921da334c5125c8c
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-12-11 19:18:21 -0500

    Created the test class for the TaskService

---
+++ TaskManager/src/test/java/com/SebsAndYepsDevelopment/TaskManager/service/TaskServiceTest.java
@@ -0,0 +1,31 @@
+package com.SebsAndYepsDevelopment.TaskManager.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TaskServiceTest {
+
+    @Test
+    void getAllTasks() {
+
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+}
